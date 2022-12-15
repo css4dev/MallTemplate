cc36d1dad8c334bac9ba4b2164da653728b12775
yazanalTrabulsi <yazan.altrabulsi2018@gmail.com>
2022-12-15 10:51:45 +0300

    sections

--- app/src/main/java/com/sawaaid/malltemplate/utils/Tools.java
+++ app/src/main/java/com/sawaaid/malltemplate/utils/Tools.java
@@ -166,6 +166,6 @@ public class Tools {
         Resources r = c.getResources();
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
-    
+
 
 }
